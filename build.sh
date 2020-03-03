a9a3dbf40f79f076ec7180af0bb23b5b6f0bb34f
Raphael Scheible <raphael.scheible@uniklinik-freiburg.de>
2020-03-03 11:54:36 +0100

    cleanup

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
+readonly DIR_SCRIPT=$(
+  cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1
+  pwd -P
+)
+
 # build all modules
-ant -f "$(pwd)/MaleRegexTree" -Dnb.internal.action.name=rebuild clean jar
-ant -f "$(pwd)/Random Regex Turtle" -Dnb.internal.action.name=rebuild clean jar
-ant -f "$(pwd)/ConsoleRegexTurtle" -Dnb.internal.action.name=rebuild clean jar
+ant -f "${DIR_SCRIPT}/MaleRegexTree" -Dnb.internal.action.name=rebuild clean jar
+ant -f "${DIR_SCRIPT}/Random Regex Turtle" -Dnb.internal.action.name=rebuild clean jar
+ant -f "${DIR_SCRIPT}/ConsoleRegexTurtle" -Dnb.internal.action.name=rebuild clean jar
